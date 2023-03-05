ade022c89f4164cc22321ae4e42f0639a3b7e3a5
meryvl <meryvalerolaguna@gmail.com>
2023-03-05 16:41:53 +0000

    commit login in progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,83 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+const Home = ()=> {
+const [inputUser , setInputUser]= useState()
+const [inputContraseña , setInputContraseña]= useState()
+const [namePerfil , setNamePerfil]=useState("Login user")
+const [fotoPerfil , setFotoPerfil] = useState("https://green.excertia.com/wp-content/uploads/2020/04/perfil-empty.png")
+
+const usuarios =[
+  {
+  Nombre: "Maria Isabel",
+  Apellidos:"Valero Laguna",
+  email:"mery@hotmail.es",
+  userName:"meryvl",
+  contraseña:"meryvl",
+  urlfoto: " https://concepto.de/wp-content/uploads/2018/10/bosque2-e1539893598295.jpg"
+  },
+  {
+      Nombre: "Antonio",
+      Apellidos:"",
+      email:"Antonio@hotmail.es",
+      userName:"Antonio",
+      contraseña:"1111",
+      urlfoto: " https://concepto.de/wp-content/uploads/2018/10/bosque2-e1539893598295.jpg"
+  }, 
+  ]
+
+
+  
+const login =(e)=>{
+  e.preventDefault()
+  
+    usuarios.forEach((user)=> {
+      if(inputUser == user.userName && inputContraseña == user.contraseña){
+        setNamePerfil(user.userName)
+        setFotoPerfil(user.urlfoto)
+        setInputUser("")
+        setInputContraseña("")
+      } 
+    });
+   
+
+  }
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div>
+<nav className=" Encabezado navbar d-flex justify-content-start navbar-dark bg-dark mx-5  ">
+<img className="iconoWeb" src="https://img.icons8.com/ios-filled/50/000000/gopro.png"/>
+<h2 className="text-white px-2">My Web</h2>
+</nav>
+
+<div className="bodyPage mx-5">
+  <div>
+    <img className="fotoPerfil m-2" src={fotoPerfil} />
+    <h3>{namePerfil}</h3>
+    </div>
+
+	  <form>
+  <div className="mb-3">
+	<h2 className="">Login Sesion {inputUser}</h2>
+    <label className="form-label">User Name</label>
+    <input type="text" className="form-control" value={inputUser} onChange={(e)=>setInputUser(e.target.value)}  />
+    
+  </div>
+  <div className=" mb-3">
+    <label for="exampleInputPassword1" className="form-label" >Contraseña</label>
+    <input type="password" className="form-control" id="Password1"value={inputContraseña} onChange={(e)=>setInputContraseña(e.target.value)} />
+  </div>
+      <a href="#">I don't have a user</a>
+    <div className=" d-flex justify-content-center">
+      <button type="submit" className="btn btn-primary px-3 " onClick={login} >Enviar</button>
+    </div>
+  </form>
+</div>
+
+  </div>
+
 	);
-};
+  }
 
 export default Home;
